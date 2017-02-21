1b79756c6267747516ad2d26c25115f67a58dc2f
Jeff Puckett <jeff@jeffpuckett.com>
2017-02-21 21:27:10 +0000

    test orchestra testbench on travis

---
+++ tests/Stubs/ServiceProvider.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace StudentAffairsUwm\Shibboleth\Tests\Stubs;
+
+class ServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+    public function boot()
+    {
+        $this->loadMigrationsFrom(realpath(__DIR__.'/../../src/database/migrations'));
+    }
+}
