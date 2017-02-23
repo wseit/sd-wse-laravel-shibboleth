8155e50315315de3660b01ac868250c7d54eaa30
Jeff Puckett <jeff@jeffpuckett.com>
2017-02-23 21:13:32 +0000

    encapsulate test Setup trait

--- tests/MigrateDatabaseTest.php
+++ tests/MigrateDatabaseTest.php
@@ -1,81 +1,12 @@
 <?php
 
-namespace Orchestra\Testbench\Tests\Databases;
-
 use Carbon\Carbon;
 use Orchestra\Testbench\TestCase;
+use StudentAffairsUwm\Shibboleth\Tests\Stubs\Setup;
 
 class MigrateDatabaseTest extends TestCase
 {
-    /**
-     * Setup the test environment.
-     */
-    public function setUp()
-    {
-        parent::setUp();
-
-        $this->artisan('migrate:refresh', ['--database' => 'testing']);
-    }
-
-    /**
-     * Define environment setup.
-     *
-     * @param  \Illuminate\Foundation\Application  $app
-     *
-     * @return void
-     */
-    protected function getEnvironmentSetUp($app)
-    {
-        $app['config']->set('database.default', 'testing');
-        $app['config']->set('database.connections.testing', [
-            'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE'),
-            'username' => env('DB_USERNAME'),
-            'password' => env('DB_PASSWORD'),
-            'charset' => 'utf8',
-            'collation' => 'utf8_unicode_ci',
-            'prefix' => '',
-            'strict' => true,
-            'engine' => null,
-        ]);
-    }
-
-    /**
-     * Get package providers.  At a minimum this is the package being tested, but also
-     * would include packages upon which our package depends, e.g. Cartalyst/Sentry
-     * In a normal app environment these would be added to the 'providers' array in
-     * the config/app.php file.
-     *
-     * @param  \Illuminate\Foundation\Application  $app
-     *
-     * @return array
-     */
-    protected function getPackageProviders($app)
-    {
-        return [
-            \StudentAffairsUwm\Shibboleth\Tests\Stubs\ServiceProvider::class,
-        ];
-    }
-
-    /**
-     * Get package aliases.  In a normal app environment these would be added to
-     * the 'aliases' array in the config/app.php file.  If your package exposes an
-     * aliased facade, you should add the alias here, along with aliases for
-     * facades upon which your package depends, e.g. Cartalyst/Sentry.
-     *
-     * @param  \Illuminate\Foundation\Application  $app
-     *
-     * @return array
-     */
-    protected function getPackageAliases($app)
-    {
-        return [
-            //'Sentry'      => 'Cartalyst\Sentry\Facades\Laravel\Sentry',
-            //'YourPackage' => 'YourProject\YourPackage\Facades\YourPackage',
-        ];
-    }
+    use Setup;
 
     /**
      * Test running migration.
