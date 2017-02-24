231a1e8bd8e908132f73987d02a73a64edda5a06
Jeff Puckett <jeff@jeffpuckett.com>
2017-02-24 00:11:33 +0000

    test stub apply entitlement

--- tests/EntitlementTest.php
+++ tests/EntitlementTest.php
@@ -17,6 +17,15 @@ class EntitlementTest extends TestCase
         new Entitlement(1);
     }
 
+    public function testCanCreateEntitlement()
+    {
+        $entitlement = "urn:mace:dir:entitlement:common-lib-terms;urn:mace:uark.edu:ADGroups:Computing Services:Bomgar Groups:Bomgar-WCOB;urn:mace:uark.edu:ADGroups:Walton College:Security Groups:Old Security Groups:WCOB-TechCenter;urn:mace:uark.edu:ADGroups:Exchange Resource Units:UITS (University IT Services):UITS: TechPartners;urn:mace:uark.edu:ADGroups:Walton College:Security Groups:WCOB-Intranet;urn:mace:uark.edu:ADGroups:walton:Groups:linux02_sudoers;urn:mace:uark.edu:ADGroups:Walton College:Security Groups:WCOB-Users;urn:mace:uark.edu:ADGroups:Exchange Resource Units:WCOB (Walton College):WCOB: Conference Team";
+
+        $entitlement = new Entitlement($entitlement);
+
+        $this->assertInstanceOf(Entitlement::class, $entitlement);
+    }
+
     public function testCanApplyEntitlements()
     {
         $user = new User;
@@ -27,6 +36,6 @@ class EntitlementTest extends TestCase
 
         $entitlement->applyTo($user);
 
-        $this->assertInstanceOf(Entitlement::class, $entitlement);
+        // TODO: fetch user groups and verify membership mapping
     }
 }
