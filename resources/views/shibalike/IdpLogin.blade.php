19b9c5d4efd27e69b0ac2786f70d5e74a49643c1
Tim Jarrett <tim.jarrett@gmail.com>
2025-07-21 08:52:28 -0400

    Brings the dropdown login form into the Laravel Shibboleth plugin

--- resources/views/shibalike/IdpLogin.blade.php
+++ resources/views/shibalike/IdpLogin.blade.php
@@ -1,74 +1,89 @@
+<!DOCTYPE html>
 <html>
-
 <head>
-	<title>Emulated IdP Login</title>
-	<style type="text/css">
-		body {
-			font-family: sans-serif;
-		}
+    <title>Emulated IdP Login</title>
+    <style type="text/css">
+        body {
+            font-family: sans-serif;
+        }
 
-		form {
-			color: grey;
-		}
+        form {
+            color: grey;
+        }
 
-		div.login-box {
-			margin: 10px auto;
-			width: 100%;
-			border: 1px solid grey;
-			border-radius: 5px;
-			padding: 10px;
-			max-width: 400px;
-			min-width: 300px;
-		}
+        div.login-box {
+            margin: 10px auto;
+            width: 100%;
+            border: 1px solid grey;
+            border-radius: 5px;
+            padding: 10px;
+            max-width: 400px;
+            min-width: 300px;
+        }
 
-		.title {
-			text-align: center;
-			font-weight: 200;
-			color: grey;
-		}
+        .title {
+            text-align: center;
+            font-weight: 200;
+            color: grey;
+        }
 
-		input[type="text"],
-		input[type="password"] {
-			width: 100%;
-			padding: 5px;
-			border-radius: 5px;
-			border: 1px solid #cdcdcd;
-		}
+        input[type="text"],
+        input[type="password"],
+        select {
+            width: 100%;
+            padding: 5px;
+            border-radius: 5px;
+            border: 1px solid #cdcdcd;
+        }
 
-		input[type="submit"] {
-			padding: 10px;
-			border: 1px solid #cdcdcd;
-			border-radius: 5px;
-			background-color: #fff;
-			min-width: 100%;
-		}
+        input[type="submit"] {
+            padding: 10px;
+            border: 1px solid #cdcdcd;
+            border-radius: 5px;
+            background-color: #fff;
+            min-width: 100%;
+        }
 
-		input[type="submit"]:hover {
-			background-color: #cdcdcd;
-			cursor: pointer;
-		}
-	</style>
+        input[type="submit"]:hover {
+            background-color: #cdcdcd;
+            cursor: pointer;
+        }
+    </style>
 </head>
-
 <body>
-	<div class="login-box">
-		<h2 class="title">Login to Continue</h2>
-		<form action="" method="post">
-			<input type="hidden" name="_token" value="<?php echo csrf_token();?>">
-			<strong>{{ $error ?? "Please login below." }}</strong>
-			<p>
-				<label for="username">Username</label>
-				<input type="text" name="username" id="username" />
-			</p>
-			<p>
-				<label for="password">Password</label>
-				<input type="password" name="password" id="password" />
-			</p>
-			<p>
-				<input type="submit" value="Login">
-			</p>
-		</form>
-	</div>
-	</div>
+<div class="login-box">
+    <h2 class="title">Login to Continue</h2>
+    <form action="" method="post">
+        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+        <strong>{{ $error ?? 'Please login below.' }}</strong>
+        <p>
+            <select id="user-select">
+                <option value="" disabled selected>Select One</option>
+                @foreach(config('shibboleth.emulate_idp_users') as $username => $info)
+                    <option value="{{ $username }}">{{ $username }} - {{ $info['displayName'] }}</option>
+                @endforeach
+            </select>
+            <input type="hidden" name="username" id="username" />
+            <input type="hidden" name="password" id="password" />
+        </p>
+        <p>
+            <input type="submit" value="Login">
+        </p>
+    </form>
+</div>
 
+<script>
+    document.addEventListener('DOMContentLoaded', function () {
+        const select = document.getElementById('user-select');
+        const usernameInput = document.getElementById('username');
+        const passwordInput = document.getElementById('password');
+
+        select.addEventListener('change', function () {
+            const selectedValue = select.value;
+            usernameInput.value = selectedValue;
+            passwordInput.value = selectedValue;
+        });
+    });
+</script>
+</body>
 </html>
